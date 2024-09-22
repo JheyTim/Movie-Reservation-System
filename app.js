2beedccab9747053c4faef8a40b91177740f6b81
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-09-23 01:19:13 +0800

    Remove dotenv on models

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const sequelize = require('./models');
 const authRoutes = require('./routes/authRoutes');
 const movieRoutes = require('./routes/movieRoutes');
 const reservationRoutes = require('./routes/reservationRoutes');
-require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
